3c4073d3315edf329cc9b8a0c5b5ad91d4d146f1
Arun Rama Reddy <arunramareddy@gmail.com>
2019-11-05 22:18:15 +0530

    feat: pong-7 collision update

--- cs50g/pong/main.lua
+++ cs50g/pong/main.lua
@@ -16,18 +16,21 @@ PADDLE_SPEED = 200
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.window.setTitle('Pong')
   math.randomseed(os.time())
 
   smallFont = love.graphics.newFont('font.ttf', 8)
 
   scoreFont = love.graphics.newFont('font.ttf', 32)
+
   love.graphics.setFont(smallFont)
-  love.window.setTitle('Pong')
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINODW_HEIGHT, {
     fullscreen = false,
     resizable = false,
     vsync = true
   })
+
   player1Score = 0
   player2Score = 0
 
@@ -52,6 +55,39 @@ end
 
 
 function love.update(dt)
+  if gameState == 'play' then
+    if ball:collides(player1) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player1.x + 5
+
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+    if ball:collides(player2) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player2.x - 4
+
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+
+    if ball.y <= 0 then
+      ball.y = 0
+      ball.dy = -ball.dy
+    end
+
+    if ball.y >= VIRTUAL_HEIGHT - 4 then
+      ball.y = VIRTUAL_HEIGHT - 4
+      ball.dy = -ball.dy
+    end
+  end
+
   if love.keyboard.isDown('w') then
     player1.dy = -PADDLE_SPEED
   elseif love.keyboard.isDown('s') then
@@ -67,8 +103,10 @@ function love.update(dt)
   if gameState == 'play' then
     ball:update(dt)
   end
+
   player1:update(dt)
   player2:update(dt)
+
 end
 
 function love.draw()

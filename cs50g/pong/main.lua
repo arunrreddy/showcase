ab3cf4da609f1e59cc7a38f67b188f4b3c9777e6
Arun Rama Reddy <arunramareddy@gmail.com>
2019-11-11 22:14:10 +0530

    feat: pong-10 victory update

--- cs50g/pong/main.lua
+++ cs50g/pong/main.lua
@@ -20,7 +20,7 @@ function love.load()
   math.randomseed(os.time())
 
   smallFont = love.graphics.newFont('font.ttf', 8)
-
+  largeFont = love.graphics.newFont('font.ttf', 16)
   scoreFont = love.graphics.newFont('font.ttf', 32)
 
   love.graphics.setFont(smallFont)
@@ -39,8 +39,10 @@ function love.load()
   player1 = Paddle(10, 30, 5, 20)
   player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
   ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
   gameState = 'start'
 end
+
 function love.update(dt)
   if gameState == 'serve' then
     ball.dy = math.random(-50, 50)
@@ -60,6 +62,7 @@ function love.update(dt)
         ball.dy = math.random(10, 150)
       end
     end
+
     if ball:collides(player2) then
       ball.dx = -ball.dx * 1.03
       ball.x = player2.x - 4
@@ -80,33 +83,31 @@ function love.update(dt)
       ball.y = VIRTUAL_HEIGHT - 4
       ball.dy = -ball.dy
     end
-  end
 
     if ball.x < 0 then
-      player2Score = player2Score + 1
-      ball:reset()
       servingPlayer = 1
-      gameState = 'serve'
+      player2Score = player2Score + 1
+      if player2Score == 10 then
+        winningPlayer = 2
+        gameState = 'gameOver'
+      else
+        gameState = 'serve'
+        ball:reset()
+      end
     end
 
     if ball.x > VIRTUAL_WIDTH then
-      player1Score = player1Score + 1
-      ball:reset()
       servingPlayer = 2
-      gameState = 'serve'
+      player1Score = player1Score + 1
+      if player1Score == 10 then
+        winningPlayer = 1
+        gameState = 'gameOver'
+      else
+        gameState = 'serve'
+        ball:reset()
+      end
     end
-
-    if player1Score == 10 then
-      gameState = 'gameover'
-      player1Score = 0
-      player2Score = 0
-    end
-
-    if player2Score == 10 then
-      gameState = 'gameover'
-      player1Score = 0
-      player2Score = 0
-    end
+  end
 
   if love.keyboard.isDown('w') then
     player1.dy = -PADDLE_SPEED
@@ -141,6 +142,16 @@ function love.keypressed(key)
       gameState = 'serve'
     elseif gameState == 'serve' then
       gameState = 'play'
+    elseif gameState == 'gameOver' then
+      gameState = 'serve'
+      ball:reset()
+      player1Score = 0
+      player2Score = 0
+      if winningPlayer == 1 then
+        servingPlayer = 2
+      else
+        servingPlayer = 1
+      end
     end
   end
 end
@@ -183,6 +194,10 @@ function love.draw()
       'center'
     )
   elseif gameState == 'play' then
+  elseif gameState == 'gameOver' then
+    love.graphics.setFont(largeFont)
+    love.graphics.printf('Player ' ..tostring(winningPlayer) ..' wins!', 0, 10, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter to restart!', 0, 30, VIRTUAL_WIDTH, 'center')
   end
   player1:render()
   player2:render()

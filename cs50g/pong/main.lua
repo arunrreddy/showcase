95f71a2399c37338e98731bff37ff9ce374e8dc3
Arun Rama Reddy <arunramareddy@gmail.com>
2019-11-12 08:01:09 +0530

    feat:pong-11 audio update,change wav files to mp3 to play on linux

--- cs50g/pong/main.lua
+++ cs50g/pong/main.lua
@@ -25,6 +25,12 @@ function love.load()
 
   love.graphics.setFont(smallFont)
 
+  sounds = {
+    ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.mp3', 'static'),
+    ['score'] = love.audio.newSource('sounds/score.mp3', 'static'),
+    ['wall_hit'] = love.audio.newSource('sounds/wall_hit.mp3', 'static')
+  }
+
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINODW_HEIGHT, {
     fullscreen = false,
     resizable = false,
@@ -61,6 +67,7 @@ function love.update(dt)
       else
         ball.dy = math.random(10, 150)
       end
+      sounds['paddle_hit']:play()
     end
 
     if ball:collides(player2) then
@@ -72,21 +79,25 @@ function love.update(dt)
       else
         ball.dy = math.random(10, 150)
       end
+      sounds['paddle_hit']:play()
     end
 
     if ball.y <= 0 then
       ball.y = 0
       ball.dy = -ball.dy
+      sounds['wall_hit']:play()
     end
 
     if ball.y >= VIRTUAL_HEIGHT - 4 then
       ball.y = VIRTUAL_HEIGHT - 4
       ball.dy = -ball.dy
+      sounds['wall_hit']:play()
     end
 
     if ball.x < 0 then
       servingPlayer = 1
       player2Score = player2Score + 1
+      sounds['score']:play()
       if player2Score == 10 then
         winningPlayer = 2
         gameState = 'gameOver'
@@ -99,6 +110,7 @@ function love.update(dt)
     if ball.x > VIRTUAL_WIDTH then
       servingPlayer = 2
       player1Score = player1Score + 1
+      sounds['score']:play()
       if player1Score == 10 then
         winningPlayer = 1
         gameState = 'gameOver'
